5cce4143dbe0c8e5e1831f5f2c4809f21d58aa1b
Bleach1 <963262676@qq.com>
2018-01-22 13:28:05 +0800

    update

--- NationalSecurityCommunity/app/src/main/java/com/national/security/community/ui/SplashActivity.java
+++ NationalSecurityCommunity/app/src/main/java/com/national/security/community/ui/SplashActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.national.security.community.R;
