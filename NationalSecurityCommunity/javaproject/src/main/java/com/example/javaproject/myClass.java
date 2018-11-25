63cb416800eb7bc1e4efebf954ca541134dda23a
Bleach1 <963262676@qq.com>
2018-11-25 08:25:49 +0800

    sssss

--- NationalSecurityCommunity/javaproject/src/main/java/com/example/javaproject/myClass.java
+++ NationalSecurityCommunity/javaproject/src/main/java/com/example/javaproject/myClass.java
@@ -1,7 +1,7 @@
 package com.example.javaproject;
 
 public class myClass {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         A a = new B();
         a = new B();
     }
